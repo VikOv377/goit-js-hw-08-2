232b5d4a6348e61a00459daaf924131539333401
Viktoriya Ovcharenko <vickymmm2012@gmail.com>
2022-10-03 10:30:11 +0200

    01-gallery.js done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector('.gallery');
+
+/*Створюємо галерею*/
+
+function createGalleryItems ({ preview, original, description}) {
+    return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+        </a>
+      </div>
+    `;
+}
+
+const addGalleryItems = galleryItems.map(createGalleryItems).join('');
+galleryContainer.insertAdjacentHTML ('beforeend', addGalleryItems);
+
+/*Добавляємо властивості галереї SimpleLightBox*/
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, scrollZoom: true, captionsData: "alt" });
 
 console.log(galleryItems);
